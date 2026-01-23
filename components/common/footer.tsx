567521caef78d528883ce148048ce23e3fa576da
thangbuiq <buiquangthangtv@gmail.com>
2026-01-23 17:48:10 +0700

    feat: remove email from footer

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -5,25 +5,26 @@ export default function Footer() {
     <footer className="mt-10 sm:mt-16 mb-20 pt-8 border-t border-[#e5e5e5] text-center text-[#787670] text-xs sm:text-sm">
       <div className="flex flex-col items-center gap-4">
         <p className="text-[#999792] leading-relaxed italic">
-          &quot;chúc bạn đọc một ngày bình yên 🌱&quot;
+          &quot;chúc bạn đọc một ngày bình yên&quot;
         </p>
 
         <div className="flex items-center gap-6 mt-2">
           <a
-            href="https://github.com/thangbuiq"
+            href="https://github.com/thangbuiq/chuyenminhke"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-[#1d1d1d] transition-colors"
           >
             github
           </a>
+          {/*
           <span className="w-1 h-1 rounded-full bg-gray-300"></span>
-          <a
-            href="mailto:thangbuiq@gmail.com"
+          <p
             className="hover:text-[#1d1d1d] transition-colors"
           >
-            contact
-          </a>
+            buiquangthangtv@gmail.com
+          </p>
+          */}
         </div>
 
         <p className="mt-4 text-[10px] uppercase tracking-widest opacity-60">
